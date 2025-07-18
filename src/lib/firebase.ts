0b328cf67af33229f20919318483792994d48195
Jeremy Bani <Jeremy.bani.fernandez@gmail.com>
2025-07-18 07:52:45 +0000

    je ne comprends pas ce qu'il te manque avec ça : // Import the functions

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,6 +3,7 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDBNZu4OZVqAQNY07FbGU5jDIQqWm4Plcg",
   authDomain: "zenergy-f8276.firebaseapp.com",
