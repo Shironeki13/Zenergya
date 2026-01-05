aeba847028ead1bfacd5cd4b73bc8733a48c8099
Shironeki13 <124183636+Shironeki13@users.noreply.github.com>
2026-01-05 10:07:14 +0100

    chore: Secure Firebase config via environment variables

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,13 +7,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDBNZu4OZVqAQNY07FbGU5jDIQqWm4Plcg",
-  authDomain: "zenergy-f8276.firebaseapp.com",
-  projectId: "zenergy-f8276",
-  storageBucket: "zenergy-f8276.appspot.com",
-  messagingSenderId: "105631849496",
-  appId: "1:105631849496:web:ba5d073bee98e99b9bf76d",
-  measurementId: "G-MHF0K9NRFP"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
